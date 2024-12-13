74d5ac5f649f29122cc441eb92e7af9adc2e5dac
ManarMashaal <manar.mashaal.studenti@isii.it>
2024-12-13 22:29:07 +0100

    tolta la navbar dal template header

--- template_header.php
+++ template_header.php
@@ -16,27 +16,4 @@
     
 </head>
 <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg fixed-top custom-navbar">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="image/logo.png" alt="Logo" width="40" height="40" class="d-inline-block align-middle"> 
-                <span>Online Learning Hub</span>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="corsi.php">Corsi</a></li>
-                    <li class="nav-item"><a class="nav-link" href="aboutUs.php">About Us</a></li>
-                    <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
-                    <li class="nav-item"><a class="nav-link" href="amministratoreDashboard.php">Amministratore</a></li>
-                    <li class="nav-item"><a class="nav-link" href="istruttoreDashboard.php">Istruttori</a></li>
-                    <li class="nav-item"><a class="nav-link" href="studentDashboard.php">Studenti</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    
