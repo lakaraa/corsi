89ace14d99d5a84099520f3ec110c4f60fc5d527
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-01-15 20:20:12 +0100

    Merge branch 'main' of https://github.com/lakaraa/corsi

--- config.php
+++ config.php
@@ -1,11 +1,33 @@
 <?php
 
-$db_host = '127.0.0.1';
-$db_dbname = 'corsi';
-$db_username = 'corsi';
-$db_password = 'password.123';
+// Definizione delle credenziali del database
+define('DB_SERVER', $_ENV['AZURE_MYSQL_HOST']);
+define('DB_USERNAME', $_ENV['AZURE_MYSQL_USERNAME']);
+define('DB_PASSWORD', $_ENV['AZURE_MYSQL_PASSWORD']);
+define('DB_NAME', $_ENV['AZURE_MYSQL_DBNAME']);
+define('SSL_CA', 'BaltimoreCyberTrustRoot.crt.pem');
 
+try {
+    // Stringa DSN per la connessione PDO
+    $dsn = "mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME;
 
-$pdo = new PDO("mysql:host=$db_host;dbname=$db_dbname", $db_username, $db_password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    // Opzioni di connessione PDO, con supporto per SSL
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,  // Modalità di gestione degli errori tramite eccezioni
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,  // Tipo di fetch dei risultati
+        PDO::ATTR_EMULATE_PREPARES => false,  // Disabilita la preparazione emulata delle query
+        PDO::MYSQL_ATTR_SSL_CA => SSL_CA,  // Certificato CA
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,  // Disabilita la verifica del certificato del server
+    ];
+
+    // Creazione della connessione PDO
+    $pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, $options);
+
+    // Se la connessione ha successo
+    echo "Connected successfully to the database!";
+} catch (PDOException $e) {
+    // In caso di errore, mostra il messaggio di errore
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+?>
