92ffa59335405edbf21e47c2c28adae8cbce8001
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-01-16 10:37:12 +0100

    Correzione query SQL e aggiornamento file di configurazione

--- config.php
+++ config.php
@@ -1,4 +1,40 @@
 <?php
+//
+//// Definizione delle credenziali del database
+//define('DB_SERVER', $_ENV['AZURE_MYSQL_HOST']);
+//define('DB_USERNAME', $_ENV['AZURE_MYSQL_USERNAME']);
+//define('DB_PASSWORD', $_ENV['AZURE_MYSQL_PASSWORD']);
+//define('DB_NAME', $_ENV['AZURE_MYSQL_DBNAME']);
+//define('SSL_CA', 'DigiCertGlobalRootG2.crt.pem');
+//
+//try {
+//    // Stringa DSN per la connessione PDO
+//    $dsn = "mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME;
+//
+//    // Opzioni di connessione PDO, con supporto per SSL
+//    $options = [
+//        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,  // Modalità di gestione degli errori tramite eccezioni
+//        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,  // Tipo di fetch dei risultati
+//        PDO::ATTR_EMULATE_PREPARES => false,  // Disabilita la preparazione emulata delle query
+//        PDO::MYSQL_ATTR_SSL_CA => SSL_CA,  // Certificato CA
+//        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,  // Disabilita la verifica del certificato del server
+//    ];
+//
+//    // Creazione della connessione PDO
+//    $pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, $options);
+//
+//    // Se la connessione ha successo
+//    //echo "Connected successfully to the database!";
+//} catch (PDOException $e) {
+//    // In caso di errore, mostra il messaggio di errore
+//    die("ERROR: Could not connect. " . $e->getMessage());
+//}
+//
+?>
+
+
+
+<?php
 
 // Definizione delle credenziali del database
 define('DB_SERVER', $_ENV['AZURE_MYSQL_HOST']);
@@ -23,11 +59,20 @@ try {
     // Creazione della connessione PDO
     $pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD, $options);
 
-    // Se la connessione ha successo
-    //echo "Connected successfully to the database!";
+    // Esegui il file SQL per la creazione delle tabelle
+    $createTablesSQL = file_get_contents('sql_create_tables.sql');
+    $pdo->exec($createTablesSQL); // Esegui il SQL per creare le tabelle
+
+    // Esegui il file SQL per l'inserimento dei dati
+    $insertDataSQL = file_get_contents('sql_insert.sql');
+    $pdo->exec($insertDataSQL); // Esegui il SQL per inserire i dati
+
+    echo "Tabelle create e dati inseriti con successo.";
+
 } catch (PDOException $e) {
     // In caso di errore, mostra il messaggio di errore
     die("ERROR: Could not connect. " . $e->getMessage());
 }
 
 ?>
+
