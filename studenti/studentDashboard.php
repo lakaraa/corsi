672daba8bbeb08643688a8f26147112f599e23eb
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-01-18 23:24:14 +0100

    aggiunta controllo session_start

--- studenti/studentDashboard.php
+++ studenti/studentDashboard.php
@@ -6,7 +6,11 @@ include('../templates/template_header.php');
 // Inizia la sessione
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); session_start();
+error_reporting(E_ALL); 
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (!isset($_SESSION['user_id'])) {
     echo "Sessione non valida.";
     exit;
