df07d98c56cbc293a7f8c790cdea677a432e4f22
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-01-22 15:35:45 +0100

    tolta print_r

--- studenti/studentDashboard.php
+++ studenti/studentDashboard.php
@@ -150,7 +150,6 @@ if (count($coursesEnrolledIds) > 0) {
                     </div>
                 </div>
             </div>
-<?php print_r($coursesAvailable);?>
             <!-- Corsi Disponibili -->
             <div class="row">
                 <div class="col-md-12">
